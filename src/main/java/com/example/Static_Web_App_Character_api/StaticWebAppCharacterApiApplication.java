5f6c428337a373c968bfde6a9324e70aa71298f0
Joseph Osoh <josephocollege@gmail.com>
2026-03-09 17:57:55 -0400

    First Spring Boot project setup

---
+++ src/main/java/com/example/Static_Web_App_Character_api/StaticWebAppCharacterApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Static_Web_App_Character_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticWebAppCharacterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticWebAppCharacterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Static_Web_App_Character_api/StaticWebAppCharacterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Static_Web_App_Character_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticWebAppCharacterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
